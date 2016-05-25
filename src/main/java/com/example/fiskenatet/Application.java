57e3b15d75594fc99f6d27fc56e7c74b328fe7ff
onordin <nordin.oscar@gmail.com>
2016-05-25 12:52:00 +0200

    procfile changed

--- src/main/java/com/example/fiskenatet/Application.java
+++ src/main/java/com/example/fiskenatet/Application.java
@@ -2,11 +2,7 @@ package com.example.fiskenatet;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.util.logging.FileHandler;
 import java.util.logging.Logger;
-import java.util.logging.SimpleFormatter;
 
 
 @SpringBootApplication
